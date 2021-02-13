71b9865d35904c05dfb461463300330832d65d5c
Shrishti-Ture <74102483+Shrishti-Ture@users.noreply.github.com>
2021-02-13 18:51:18 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+var home = document.getElementById("home");
+var disablity = document.getElementById("disablity");
+var help = document.getElementById("help");
+var contact = document.getElementById("contact");
+
+
+
+home.onclick=()=>{
+    location.href = "./index.html"
+};
+
+disablity.onclick=()=>{
+    location.href = "./disablity.html"
+};
+
+help.onclick=()=>{
+    location.href = "./help.html"
+};
+
+contact.onclick=()=>{
+    location.href = "./contact.html"
+};
+
